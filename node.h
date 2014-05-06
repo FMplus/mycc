fa092e65d601510584fafd8c9a283eef70a803d5
fmplus <fmplus@gmail.com>
2014-05-06 20:48:46 +0800

    add note.h to creat the parser tree

---
+++ node.h
@@ -0,0 +1,21 @@
+#ifndef _NODE_H_
+#define _NODE_H_
+#include <iostream>
+#include <stdlib.h>
+#include <stack>
+#include <vector>
+
+struct node
+{
+    typedef std::string string;
+    string code;
+    string res;
+    bool isID;
+    bool isNUM;
+    bool isREG;
+    bool isREGID;
+};
+
+extern   stack<node> nodelist;
+
+#endif
